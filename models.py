a514b438f17bd949643b7787483d1b78c7291838
Miguel Escribano (miguescri) <miguescri@gmail.com>
2021-05-15 04:56:45 +0200

    Add base database models for the use case
    - User: contains user login info
    - Meeting: refers to a meeting created by a user
    - Participant: relationship between a user and a meeting

---
+++ models.py
@@ -0,0 +1,49 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, ForeignKey, String, DateTime
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
+Session = sessionmaker()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    email = Column(String, primary_key=True)
+    name = Column(String)
+    hashed_password = Column(String)
+
+
+class Meeting(Base):
+    __tablename__ = 'meetings'
+
+    id = Column(String, primary_key=True)
+    datetime = Column(DateTime)
+
+
+class Participant(Base):
+    __tablename__ = 'participants'
+
+    user_id = Column(String, ForeignKey('users.email'), primary_key=True)
+    meeting_id = Column(String, ForeignKey('meetings.id'), primary_key=True)
+
+    user = relationship('User', back_populates='participants')
+    meeting = relationship('Meeting', back_populates='participants')
+
+
+User.participants = relationship('Participant')
+Meeting.participants = relationship('Participant')
+
+
+def create_db(database: str) -> None:
+    engine = create_engine(database)
+    Base.metadata.create_all(engine)
+
+
+def bind_engine(database: str) -> None:
+    engine = create_engine(database)
+    Session.configure(bind=engine)
+
+
+def get_session() -> Session:
+    return Session()
