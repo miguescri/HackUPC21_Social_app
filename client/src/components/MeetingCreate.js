a05bd7ddc1a6607a47a14220cc5e9ade68c92afa
Miguel Escribano (miguescri) <miguescri@gmail.com>
2021-05-16 02:39:25 +0200

    Views for Create Meeting

--- client/src/components/MeetingCreate.js
+++ client/src/components/MeetingCreate.js
@@ -1,11 +1,67 @@
 import React, {useState} from "react";
 
-function MeetingCreate() {
+function MeetingCreate({meetingCreated, createMeeting, leaveCreated}) {
+    const [hours, setHours] = useState(0)
+    const [location, setLocation] = useState('')
+    const [subject, setSubject] = useState('')
+    let component
+
+    if (meetingCreated) {
+        component = (
+            <div>
+                <h2>Meeting created</h2>
+                <div>Id: {meetingCreated['id']}</div>
+                <div>Location: {meetingCreated['location']}</div>
+                <div>Subject: {meetingCreated['subject']}</div>
+
+                <button onClick={(e) => {
+                    e.preventDefault()
+                    leaveCreated()
+                }}>
+                    Leave
+                </button>
+            </div>
+        )
+    } else {
+        component = (
+            <div>
+                <h2>
+                    Create a meeting
+                </h2>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    createMeeting(hours, location, subject)
+                }}>
+                    <div>
+                        Hours:
+                        <input
+                            type="number"
+                            value={hours}
+                            onChange={(e) => setHours(Number(e.target.value))}/>
+                    </div>
+                    <div>
+                        Location:
+                        <input
+                            type="text"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}/>
+                    </div>
+                    <div>
+                        Subject:
+                        <input
+                            type="text"
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}/>
+                    </div>
+                    <input type="submit" value="Create"/>
+                </form>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h2>
-                Create a meeting
-            </h2>
+            {component}
         </div>
     )
 }
--- client/src/components/MeetingView.js
+++ client/src/components/MeetingView.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import MeetingCreate from "./MeetingCreate";
 import MeetingList from "./MeetingList";
 import MeetingJoin from "./MeetingJoin";
-import {get_meetings} from "../endpoints"
+import {create_meeting, get_meetings} from "../endpoints"
 
 const ACTION_LIST = 0
 const ACTION_CREATE = 1
@@ -11,14 +11,18 @@ const ACTION_JOIN = 2
 function MeetingView({token}) {
     const [action, setAction] = useState(ACTION_LIST)
     const [meetingsList, setMeetingsList] = useState([])
+    const [meetingCreated, setMeetingCreated] = useState(null)
     let actionComponent
 
     const refresh = () => get_meetings(token, setMeetingsList)
+    const create = (hours, location, subject) => create_meeting(token, hours, location, subject, setMeetingCreated)
+    const leave_created = () => setMeetingCreated(null)
 
     if (action === ACTION_LIST) {
         actionComponent = <MeetingList meetings={meetingsList} refresh={refresh}/>
     } else if (action === ACTION_CREATE) {
-        actionComponent = <MeetingCreate/>
+        actionComponent =
+            <MeetingCreate meetingCreated={meetingCreated} createMeeting={create} leaveCreated={leave_created}/>
     } else if (action === ACTION_JOIN) {
         actionComponent = <MeetingJoin/>
     }
